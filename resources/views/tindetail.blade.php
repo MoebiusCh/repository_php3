5203fdfda23d6335fa23447c1b8df06cdd419ecf
千早 <phathentaidz2004@gmail.com>
2024-05-17 17:44:30 +0700

    lab 2

--- resources/views/tindetail.blade.php
+++ resources/views/tindetail.blade.php
@@ -3,10 +3,22 @@
 @section('title', 'Tin tuc')
 
 @section('content')
-    <h1 class="text-2xl font-bold"></h1>
-    <span></span>
-    <br />
-    <em>Ngày đăng:</em>
-
-    <hr />
+    <div class="min-h-dvh">
+        <h1 class="text-3xl font-bold">Tin tức {{ $tintuc->tieuDe }}</h1>
+        <div class="card card-side border-zinc-950 bg-base-100 shadow-xl">
+            <figure>
+                <img src="" alt="Movie" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title text-2xl font-bold">
+                    <a href="{{ route('tindetail', ['id' => $tintuc->id]) }}">{{ $tintuc->tieuDe }}</a>
+                </h2>
+                <p><span>{{ $tintuc->noiDung }}</span></p>
+                <div class="card-actions justify-end">
+                    <em>Ngày đăng: {{ $tintuc->created_at }}</em>
+                    <button class="btn btn-primary">Watch</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
